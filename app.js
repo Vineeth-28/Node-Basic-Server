2b98872131be3c59645d0e14dc999733d2aabdc0
Vineeth-28 <vineetprakash918@gmail.com>
2026-05-29 06:51:05 -0700

    added .gitignore

---
+++ app.js
@@ -0,0 +1,16 @@
+// server.js
+const http = require("http");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js Server!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
