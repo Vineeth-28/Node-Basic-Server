9a9e0dd671ba6bdd3b7a37d045477897a50757e8
Vineet <vineetprakash918@gmail.com>
2026-05-30 06:21:23 -0700

    Change server response message in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node.js Server!");
+  res.end("Hello from Node.js !");
 });
 
 const PORT = process.env.PORT || 3000;
